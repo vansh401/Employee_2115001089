fb4a3f9d49c056c2ba89667f0be727b734de3cca
Vansh Verma <106549024+vansh401@users.noreply.github.com>
2025-03-21 15:46:10 +0530

    [Vansh Verma] Add: UC3 Task Completed

--- Employee.js
+++ Employee.js
@@ -32,3 +32,23 @@ switch (empCheck) {
 
 let empWage = empHrs * WAGE_PER_HOUR;  //empWage = 80, empHrs = 4, WAGE_PER_HOUR = 20;
 console.log("Emp Wage: " + empWage);
+
+// UC3
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+
+ empHrs = 0;
+ empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+
+ empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
